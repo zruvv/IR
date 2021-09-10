67e69cd917b28f552f471891b635f062b5615d79
Michael Wurz <mwurz@proarch.com>
2021-09-10 09:01:52 -0400

    Update Collect-The-Windows.ps1

--- Forensics/Collect-The-Windows.ps1
+++ Forensics/Collect-The-Windows.ps1
@@ -15,9 +15,17 @@ Write-Host "
 | |     ___ | | | ___  ___| |_ ______| |  | |__   ___ ______ \  /\  / / _ _ __   __| | _____      _____ 
 | |    / _ \| | |/ _ \/ __| __|______| |  | '_ \ / _ \______\ \/  \/ / | | '_ \ / _` |/ _ \ \ /\ / / __|
 | |____ (_) | | |  __/ (__| |_       | |  | | | |  __/       \  /\  /  | | | | | (_| | (_) \ V  V /\__ \
-\ _____\___/|_|_|\___|\___|\__|      |_|  |_| |_|\___|        \/  \/   |_|_| |_|\__,_|\___/ \_/\_/ |___/    " -ForegroundColor Cyan
+\ _____\___/|_|_|\___|\___|\__|      |_|  |_| |_|\___|        \/  \/   |_|_| |_|\__,_|\___/ \_/\_/ |___/    `n" -ForegroundColor Cyan
 
 Write-Host "Run as Administrator to collect data from all areas (example: Security event logs)`n"
+
+Write-Host "Checking for elevated permissions..."
+$identity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+$principal = New-Object System.Security.Principal.WindowsPrincipal($identity)
+if ($principal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator))
+    {Write-Host "Script is running as administrator" -ForegroundColor Gray }
+else
+    {Write-Warning "Script is not running as administrator, you will not collect all relevant data!" }
 
 #Collect Scheduled Tasks
 Write-Host "Collecting Scheduled Tasks"
@@ -36,12 +44,12 @@ Write-Host "Applications Collected`n" -ForegroundColor Green
 
 #Collect Services
 Write-Host "Collecting Services"
-net start >> net_started-services.log
+net start > net_started-services.log
 Write-Host "Services Collected`n" -ForegroundColor Green
 
 #Collect list of running processes with their associated services in the command prompt
 Write-Host "Collecting Running Processes with Services & their executed commands"
-tasklist /svc >> tasklist_with_services_command.log
+tasklist /svc > tasklist_with_services_command.log
 Write-Host "Running Processes Collected`n" -ForegroundColor Green
 
 #Collect AutoStart applications
@@ -51,23 +59,23 @@ Write-Host "AutoStart Applications Collected`n" -ForegroundColor Green
 
 #Review Registry Entries
 Write-Host "Collecting Startup Registry Keys Applications"
-reg query HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run >> reg_local_machine_run.log
-reg query HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run >> reg_current_user_run.log
+reg query HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run > reg_local_machine_run.log
+reg query HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run > reg_current_user_run.log
 Write-Host "Startup Registry Keys Collected`n" -ForegroundColor Green
 
 #Collect Local Accounts & Groups
 Write-Host "Collecting Local Accounts & Groups Applications"
-net user >> net_user.log
-net localgroup administrators >> local_admins_group.log
-Get-LocalUser >> Get-LocalUser.log
+net user > net_user.log
+net localgroup administrators > local_admins_group.log
+Get-LocalUser > Get-LocalUser.log
 Write-Host "Local Accounts & Groups Collected`n" -ForegroundColor Green
 
 
 #Collect Network Connections
 Write-Host "Collecting Network Connections"
-netstat -ano >> netstat_ano.log
+netstat -ano > netstat_ano.log
 #Display executables involved in creating connection
-netstat -anb >> netstat_anb.log
+netstat -anb > netstat_anb.log
 #Reduce scope by removing local host connections from view
 #Get-NetTCPConnection -LocalAddress 192.168.0.14 | Sort-Object LocalPort
 Write-Host "Network Connections Collected`n" -ForegroundColor Green
@@ -75,16 +83,16 @@ Write-Host "Network Connections Collected`n" -ForegroundColor Green
 
 #Collect Firewall Settings
 Write-Host "Collecting Firewall Settings"
-netsh advfirewall show currentprofile >> firewall_profiles.log
+netsh advfirewall show currentprofile > firewall_profiles.log
 #View firewall configurations for inbound and outbound rules
-netsh firewall show config >> firewall_rules.log
+netsh firewall show config > firewall_rules.log
 Write-Host "Firewall Settings Collected`n" -ForegroundColor Green
 
 #Review SMB Sharing/Sessions
 Write-Host "Collecting SMB Data"
 Get-SMBShare | Export-Csv SMBShares.csv
-net use >> smb_shared_resources.log
-net session >> smb_sessions.log
+net use > smb_shared_resources.log
+net session > smb_sessions.log
 Write-Host "SMB Data Collected`n" -ForegroundColor Green
 
 Write-Host "Copying Raw .evtx Event Log files"
